eeb9c45a83f436a493ca1e34d75ca5b4fb0e0e55
Thilo Wening <thilo.wening@netways.de>
2024-05-21 18:15:11 +0200

    update script

--- workshop/setup.d/prepare-ansible.sh
+++ workshop/setup.d/prepare-ansible.sh
@@ -3,3 +3,13 @@
 # Create homedir
 
 mkdir -p /home/eduk8s/playbooks
+
+cat <<EOF> /home/eduk8s/clouds.yaml
+clouds:
+  training:
+    auth:
+      auth_url: "https://cloud.netways.de:5000/v3/"
+      project_domain_name: "Default"
+      user_domain_name: "Default"
+    auth_type: v3password
+EOF
